8ee4794d8b3e24a77b916e78e6652016cb6dbeb0
shrey96goyal <sgoyal@umass.edu>
2023-05-12 20:24:53 -0400

    sample astar impl, load elevation data

--- src/basic.py
+++ src/basic.py
@@ -1,9 +1,56 @@
 import osmnx as ox
 from flask import Flask, request
 from model.LocationDetails import getCommonLocation, convertLocToStr
-from model.GraphUtils import getGraphForLocation, getNearestNodes, getRouteDistance, getShortestRoute
+from model.GraphUtils import getGraphForLocation, getNearestNodes, getRouteDistance, getShortestRoute, getEdgeLength, getEdgeAbsElevation
 
 app = Flask(__name__)
+
+import heapq
+from geopy import distance
+def aStarSearch(G, origNode, destNode, shortestDistance, distRestriction, minElevation = 1):
+
+    maxDistance = shortestDistance * (1 + distRestriction)
+    print('shortest distance is ' + str(shortestDistance))
+    print('max distance is ' + str(maxDistance))
+    s = {}
+    openList = [(0, origNode, [origNode], 0, 0)]
+    possiblePaths = []
+    destNodeData = G.nodes(data=True)[destNode]
+    while len(openList) > 0 and openList[0][3] <= maxDistance:
+        f, node, path, g, dist = openList[0]
+        print(str(len(openList)) + 'hi')
+        openList.pop(0)
+        nodeData = G.nodes(data=True)[node]
+        if node == destNode:
+            print('adding')
+            # path.append(node)
+            possiblePaths = path
+            print(dist)
+            if dist <= maxDistance:
+                break
+        else:
+            s[node] = (g, dist)
+            for edges in G.out_edges(node):
+                nextNode = edges[1]
+                if nextNode in path:
+                    continue
+                nextG = g + getEdgeAbsElevation(G, node, nextNode)*minElevation
+                nextDist = dist + getEdgeLength(G, node, nextNode)
+                if nextDist > maxDistance:
+                    continue
+                if nextNode not in s or s[nextNode][0] > nextG or s[nextNode][1] > nextDist:
+                    nextNodeData = G.nodes(data=True)[nextNode]
+                    nextNodePath = path.copy()
+                    nextF = nextG
+                    if nextNode != destNode:
+                        nextF += minElevation*(abs(destNodeData['elevation'] - nextNodeData['elevation'])/(distance.distance((nextNodeData['y'], nextNodeData['x']), (destNodeData['y'], destNodeData['x'])).km * 1000))
+                    nextNodePath.append(nextNode)
+                    openList.append((nextF, nextNode, nextNodePath, nextG, nextDist))
+
+        heapq.heapify(openList)
+
+    print(len(possiblePaths))
+
 
 @app.route("/")
 def home():
@@ -15,17 +62,18 @@ def home():
     dest_node = getNearestNodes(G, destination[1], destination[0])
     # print(orig_node)
     # print(dest_node)
+    for edges in G.out_edges(orig_node):
+        n1 = edges[1]
+
+
+
     shortest_route = getShortestRoute(G, orig_node, dest_node)
     shortest_path_length = getRouteDistance(G, shortest_route)
+    aStarSearch(G, orig_node, dest_node, shortest_path_length, 0.3, -1)
     # print(shortest_path_length)
+    #7184243985
     return "Hello, World!"
 
-@app.route("/salvador")
-def salvador():
-    # http://127.0.0.1:5000/salvador?name=abc
-    print(request.args.get("name"))
-
-    return {1:"Hello, Salvador"}
 
 if __name__ == "__main__":
     app.run()
--- src/model/GraphUtils.py
+++ src/model/GraphUtils.py
@@ -1,6 +1,7 @@
 import osmnx as ox
 import networkx as nx
 from model.LocationDetails import convertLocToStr
+import os
 
 G = ox.graph_from_place('Sutherland Shire Council', network_type='walk')
 
@@ -12,10 +13,24 @@ def getGraphForLocation(location):
     assert type(location) is dict
     if not 'city' in location or location['city'] == '':
         return None
-    locationString = convertLocToStr(location)
-    G = ox.graph_from_place(locationString, network_type='walk', simplify=False)
+
+    apiKey = 'AIzaSyCLWLZWWVKz107DGJoCh64jj0zs8gU9YnU'
+    if os.path.isfile(location['city'] + '.graphml'):
+        print('Loading local file')
+        G = ox.load_graphml(location['city'] + '.graphml')
+    # x = ox.load_graphml('a.graphml')
+    else:
+        locationString = convertLocToStr(location)
+        G = ox.graph_from_place(locationString, network_type='drive', simplify=False)
+        # Load Elevation Data
+        G = ox.elevation.add_node_elevations_google(G, api_key=apiKey)
+        G = ox.elevation.add_edge_grades(G)
+
+        # Save
+        ox.save_graphml(G, location['city'] + ".graphml")
+
+
     # print("Here")
-    ox.save_graphml(G, "Amherst.graphml")
     # G1 = ox.add_node_elevations_google(G, '', url_template='https://api.open-elevation.com/api/v1/lookup?locations={}&key={}')
     return G
 
@@ -30,6 +45,8 @@ def getShortestRoute(Graph, source, target):
 
 def getEdgeLength(Graph, source, target):
     return Graph.get_edge_data(source, target)[0]['length']
+def getEdgeAbsElevation(Graph, source, target):
+    return Graph.get_edge_data(source, target)[0]['grade_abs']
 
 def getRouteDistance(Graph, route):
     numNodes = len(route)
