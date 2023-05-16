1f614524f249c7e65768bbb78b4852ae7244f1e5
Shaurya Rawat <realshauryarawat@hotmail.com>
2023-05-15 20:26:22 -0400

    map autocomplete
    added autocomplete plug-in for start and end locations

--- mapper.js
+++ mapper.js
@@ -5,6 +5,55 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
+var start_input = document.querySelector('#start');
+var end_input = document.querySelector('#end');
+
+var start_loc = L.Control.geocoder({
+  defaultMarkGeocode: false,
+  geocoder: L.Control.Geocoder.nominatim({
+    geocodingQueryParams: { limit: 5 },
+  }),
+  geocoder_autocomplete: true,
+}).on('markgeocode', function (x) {
+  var lat_long = x.geocode.center;
+  var loc_name = x.geocode.name;
+  start_input.value = loc_name;
+}).addTo(map);
+
+var end_loc = L.Control.geocoder({
+  defaultMarkGeocode: false,
+  geocoder: L.Control.Geocoder.nominatim({
+    geocodingQueryParams: { limit: 5 },
+  }),
+  geocoder_autocomplete: true,
+}).on('markgeocode', function (x) {
+  var lat_long = x.geocode.center;
+  var loc_name = x.geocode.name;
+  end_input.value = loc_name;
+}).addTo(map);
+
+start_input.addEventListener('focus', function () {
+  start_loc.addTo(map);
+});
+
+
+end_input.addEventListener('focus', function () {
+  end_loc.addTo(map);
+});
+
+/*
+start_input.addEventListener('blur', function () {
+  start_container.classList.remove('active');
+  start_container.removeChild(start_loc.getContainer());
+});
+
+end_input.addEventListener('blur', function () {
+  end_container.classList.remove('active');
+  end_container.removeChild(end_loc.getContainer());
+});
+*/
+
+
 
 function elevation () {
     let selectedEle = $("input[name='eleType']:checked").val();
