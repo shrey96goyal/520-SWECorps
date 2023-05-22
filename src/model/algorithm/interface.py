39b5679e11c99e2879bda8b0727f1c94f92aaadc
Riya Singh <riyasingh@Riyas-MacBook-Pro.local>
2023-05-21 21:56:29 -0400

    restructuring code, adding comments

--- src/model/algorithm/interface.py
+++
@@ -1,4 +0,0 @@
-class SearchAlgorithmInterface:
-    def search(self, graph, origNode, destNode, shortestDistance, distRestriction, calcMinElevation=True):
-        """Search for path"""
-        pass
